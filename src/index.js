057717e5b6ebf1366b6edfdc986c0d809534e2ea
YagovkinDmytro <139924381+YagovkinDmytro@users.noreply.github.com>
2024-05-13 23:35:07 +0200

    main structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './components/App/App';
+// import { Provider } from 'react-redux';
+// import { store } from './redux/store';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +14,12 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// {
+//   /* <Provider store={store}>
+//   <React.StrictMode>
+//     <BrowserRouter basename="/pickup-campers">
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// </Provider> */
+// }
